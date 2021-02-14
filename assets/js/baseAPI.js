8b6a6e3a527cdc0850afe0cedeb7400b9a3c96ea
whh <whh@itcast.cn>
2021-02-14 18:14:43 +0800

    大事件后台主页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,22 @@
 var baseAPI = 'http://api-breakingnews-web.itheima.net'
 $.ajaxPrefilter (function(options){
     options.url = baseAPI + options.url
+
+    // 为有权限的接口，设置headers请求头
+    if(options.url.indexOf('/my/') !== -1){
+        options.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+
+    // 不论登录成功或者失败，都调用complete
+    options.complete = function(res){
+        // console.log(res);
+        if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+            // 强制清空token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+$(function(){
+
+    // 获取用户的基本信息
+    getUserInfo()
+    function getUserInfo(){
+        $.ajax({
+            method: "GET",
+            url: "/my/userinfo",
+            success: function (res) {
+            if(res.status !== 0){
+                return layui.layer.msg(res.message)
+            }
+            renderAvatar(res.data)
+            }
+        });
+    }
+
+    // 渲染用户的头像
+    function renderAvatar(user){
+        // 1.获取用户的名称
+        var name = user.nickname || user.username
+        // 2.设置欢迎的文本
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        // 3.按需渲染用户头像
+        if(user.user_pic !== null){
+            $('.layui-nav-img').attr('src',user.user_pic).show()
+            $('.text-avatar').hide()
+        }else{
+            $('.layui-nav-img').hide()
+            var first = name[0].toUpperCase()
+            $('.text-avatar').html(first).show()
+        }
+    }
+
+    // 实现退出功能
+    var layer = layui.layer
+    $('#btnLogout').on('click',function(){
+        // 弹出询问框
+        layer.confirm('确认退出登录?', {icon: 3, title:'提示'}, function(index){
+            //清空本地存储中的token
+            localStorage.removeItem('token')
+            // 重新跳转到登陆页面
+            location.href = '/login.html'
+
+            // 关闭询问框
+            layer.close(index);
+        });
+    })
+
+    
+
+})
